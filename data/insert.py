d0ed2a8aa37383e117a9dde0d1a605e8d6d80840
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-08-19 20:47:45 +0900

    fix: S2의 URL을 저장하도록 변경

--- data/insert.py
+++ data/insert.py
@@ -1,13 +1,30 @@
+from pickle import APPEND
 import pandas as pd
 import json
 from collections import OrderedDict
 import glob
 import random
 
+# https://ll-inha-hkt.s3.ap-northeast-2.amazonaws.com/artist_img/1.jpg
+imgUrl='https://ll-inha-hkt.s3.ap-northeast-2.amazonaws.com'
+
 ALL_MEMBERS=[]
 MEMBER_GROUP={} # 멤버-그룹 매핑
 ARTIST_ID={} # 아티스트 PK
-MUSIC_ALBUM={}
+MUSIC_ALBUM={ # "artist" : "album"
+  1:[2, 3],
+  2:[4, 5, 6],
+  3:[7, 8],
+  4:[9, 10, 11],
+  5:[12, 13, 14],
+  6:[15, 16],
+  7:[17, 18, 19],
+  8:[31, 32, 33], # itzy
+  9:[20, 21],
+  10:[28, 29, 30],
+  11:[22, 23, 24],
+  12:[25, 26, 27],
+}
 
 group_mem_txt=open('group_members.txt','r')
 group_mem_csv=pd.read_csv(group_mem_txt, sep='\t')
@@ -26,7 +43,6 @@ def makeJsonData_Artist():
   agency_list=artist_xlsx['agency']
   color1_list=artist_xlsx['color1']
   color2_list=artist_xlsx['color2']
-  image_list=glob.glob('/media/artist_img/*.jpg') # 경로 확인 필요
 
   for id, artist, agency, img, color1, color2 in zip(id_list, artist_list, agency_list, image_list, color1_list, color2_list):
     artist_data=OrderedDict()
@@ -35,8 +51,8 @@ def makeJsonData_Artist():
       'id' : id,
       'artist_name' : artist,
       'agency' : agency,
-      'artist_image' : f'/media/artist_img/{id}.jpg',
-      'logo_image' : f'/media/artist_img/{id}.jpg', # 추후 로고 이미지로 변경 필요
+      'artist_image' : f'{imgUrl}/artist_img/{id}.jpg',
+      'logo_image' : f'{imgUrl}/artist_img/{id}.jpg', # 추후 로고 이미지로 변경 필요
       'gradient_color_1' : color1,
       'gradient_color_2' : color2,
     }
@@ -62,12 +78,11 @@ def makeJsonData_Album():
   artist_list=album_xlsx['아티스트']
   price_withT_list=album_xlsx['응모권 포함 가격']
   price_withoutT_list=album_xlsx['응모권 포함 X 가격']
-  image_list=glob.glob(f'/media/sang_album_img/*.jpg') # 경로 확인 필요
 
-  for id, album, agency, artist, albumType, year, month, priceWithT, priceWithout, img in zip(album_id_list, album_name_list, agency_list, 
+  for id, album, agency, artist, albumType, year, month, priceWithT, priceWithout in zip(album_id_list, album_name_list, agency_list, 
   artist_list, album_type_list, 
   created_year_list, created_month_list, # music_list,
-  price_withT_list, price_withoutT_list, image_list):
+  price_withT_list, price_withoutT_list):
 
     album_data=OrderedDict()
     album_data["model"]="album.Album"
@@ -78,7 +93,7 @@ def makeJsonData_Album():
       'created_at' : f"{year}-{month}-1",
       'artist' : ARTIST_ID.get(artist), # fk
       'album_type' : albumType,
-      'album_image' : f'/media/sang_album_img/{id}.jpg',
+      'album_image' : f'{imgUrl}/sang_album_img/{id}.jpg',
       'music_list' : '',
       'price_with_ticket' : priceWithT,
       'price_without_ticket' : priceWithout,
@@ -96,7 +111,7 @@ def makeJsonData_Music():
   music_xlsx=pd.read_excel('album.xlsx').fillna('missing') # NaN 처리
   music_name_list=[]
   music_playtime_list=[]
-  music_xlsx
+  music_xlsx['앨범']
 
   for i in range(1, 8): 
     music_name_list.extend(music_xlsx[f'수록곡{i}'])
@@ -111,6 +126,7 @@ def makeJsonData_Music():
         'play_time' : str(playtime),
       }
       music_data_list.append(music_data)
+
       
     with open('music-data.json', 'w', encoding="utf-8") as make_file:
       json.dump(music_data_list, make_file, ensure_ascii=False, indent="\t")
@@ -121,28 +137,31 @@ def makeJsonData_Music():
 # 포토카드 객체 JSON 데이터 생성
 def makeJsonData_Photocard():
   photocard_data_list=[]
-  image_list=glob.glob('/media/artist_img/*.jpg') # 경로 확인 필요
+  id_list=[]
+  for i in range(1, 1000) : id_list.append(i)
 
   album_xlsx=pd.read_excel('album.xlsx')
 
   album_name_list=album_xlsx['앨범명']
   artist_list=album_xlsx['아티스트']
 
-  for Photoimg in image_list: # 하나의 포토카드 이미지에 대해
-    for album in album_name_list: # 앨범 개수만큼의 포토카드 객체 생성
+  i=0
+  for album in album_name_list:
+    for img in image_list:
         photocard_data=OrderedDict()
-        name=Photoimg[16:-5]
+        name=img[16:-5]
 
         photocard_data["model"]="album.Photocard"
         photocard_data["fields"]={
-          'id' : 1,
-          'album_id' : random.randrange(2, 30), # fk
+          'id' : id_list[i],
           'artist' : MEMBER_GROUP[name], # fk
-          'img' : Photoimg,
+          'album_id' : random.choice(MUSIC_ALBUM[MEMBER_GROUP[name]]), # fk
+          'img' : f'{imgUrl}/sang_photocard_img/{}',
           'name' : name,
         }
         # if name not in ALL_MEMBERS: ALL_MEMBERS.append(Photoimg[16:-5])
         photocard_data_list.append(photocard_data)
+        i+=1
       
     with open('photocard-data.json', 'w', encoding="utf-8") as make_file:
       json.dump(photocard_data_list, make_file, ensure_ascii=False, indent="\t")
@@ -150,6 +169,6 @@ def makeJsonData_Photocard():
 
 # 각 모델에 대해 makeJsonData 함수 실행
 makeJsonData_Artist()
-makeJsonData_Music()
+#makeJsonData_Music()
 makeJsonData_Album()
 makeJsonData_Photocard()
